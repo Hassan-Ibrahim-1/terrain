2eb2d44451a6f24a4c44ca920f062341d1ef7064
Hassan-Ibrahim-1 <hassanibrahimasi@gmail.com>
2025-08-17 12:51:33 -0400

    it builds on linux

--- build.zig
+++ build.zig
@@ -11,68 +11,54 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
+    const imgui_dep = b.dependency("cimgui", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const imgui_config = cimgui.getConfig();
+    const imgui_lib = imgui_dep.artifact(imgui_config.clib_name);
+    exe.linkLibrary(imgui_lib);
+
+    exe.addIncludePath(imgui_dep.path(imgui_config.include_dir));
+    exe.addIncludePath(imgui_dep.path(imgui_config.backend_include_dir));
+    exe.addIncludePath(b.path("dependencies/stb"));
+    exe.addIncludePath(b.path("dependencies/noise"));
+    exe.addIncludePath(b.path("dependencies/glad/include"));
+
+    exe.linkSystemLibrary("assimp");
+    exe.linkSystemLibrary("jpeg");
+    exe.linkSystemLibrary("glfw");
+
     const cfiles: []const []const u8 = &.{
         "src/app.cpp",
-        "src/app.hpp",
         "src/camera.cpp",
-        "src/camera.hpp",
         "src/circle.cpp",
-        "src/circle.hpp",
         "src/color.cpp",
-        "src/color.hpp",
-        "src/common.hpp",
-        "src/cube.hpp",
         "src/debug.cpp",
-        "src/debug.hpp",
         "src/draw_command.cpp",
-        "src/draw_command.hpp",
         "src/engine.cpp",
-        "src/engine.hpp",
         "src/framebuffer.cpp",
-        "src/framebuffer.hpp",
         "src/fs.cpp",
-        "src/fs.hpp",
         "src/game_object.cpp",
-        "src/game_object.hpp",
         "src/grid.cpp",
-        "src/grid.hpp",
         "src/input.cpp",
-        "src/input.hpp",
-        "src/keycodes.hpp",
         "src/light.cpp",
-        "src/light.hpp",
         "src/line.cpp",
-        "src/line.hpp",
         "src/main.cpp",
-        "src/material.hpp",
-        "src/mesh.cpp",
-        "src/mesh.hpp",
-        "src/model.cpp",
-        "src/model.hpp",
-        "src/point.cpp",
-        "src/point.hpp",
-        "src/rect.hpp",
-        "src/renderer.cpp",
-        "src/renderer.hpp",
-        "src/scene.cpp",
-        "src/scene.hpp",
-        "src/shader.cpp",
-        "src/shader.hpp",
-        "src/skybox.cpp",
-        "src/skybox.hpp",
-        "src/sphere.hpp",
         "src/terrain.cpp",
-        "src/terrain.hpp",
+        "src/mesh.cpp",
+        "src/model.cpp",
+        "src/point.cpp",
+        "src/renderer.cpp",
+        "src/scene.cpp",
+        "src/shader.cpp",
+        "src/skybox.cpp",
         "src/texture2d.cpp",
-        "src/texture2d.hpp",
         "src/transform.cpp",
-        "src/transform.hpp",
         "src/utils.cpp",
-        "src/utils.hpp",
         "src/vertex.cpp",
-        "src/vertex.hpp",
         "src/window.cpp",
-        "src/window.hpp",
 
         "dependencies/glad/src/glad.c",
     };
@@ -86,24 +72,11 @@ pub fn build(b: *std.Build) void {
 
     exe.addCSourceFiles(.{ .files = cfiles, .flags = flags });
 
+    exe.addCSourceFile(.{ .file = imgui_dep.path(cimgui.glfwBackendSource()) });
+    exe.addCSourceFile(.{ .file = imgui_dep.path(cimgui.openGlBackendSource()) });
+
     exe.linkLibC();
     exe.linkLibCpp();
-
-    exe.linkSystemLibrary("glfw");
-
-    const imgui_dep = b.dependency("cimgui", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const imgui_config = cimgui.getConfig();
-    const imgui_lib = imgui_dep.artifact(imgui_config.clib_name);
-    exe.linkLibrary(imgui_lib);
-    exe.addIncludePath(imgui_dep.path(imgui_config.include_dir));
-
-    exe.addIncludePath(b.path("dependencies/stb"));
-    exe.addIncludePath(b.path("dependencies/noise"));
-    exe.addIncludePath(b.path("dependencies/glad/include"));
 
     b.installArtifact(exe);
 
@@ -111,4 +84,3 @@ pub fn build(b: *std.Build) void {
     const run_app_step = b.step("run", "Run the app module");
     run_app_step.dependOn(&run_app.step);
 }
-
--- src/window.cpp
+++ src/window.cpp
@@ -1,70 +1,67 @@
-#include <glad/glad.h>
 #include "debug.hpp"
+#include <glad/glad.h>
 
 #include "window.hpp"
 
-static void default_framebuffer_size_callback(GLFWwindow* window, int width, int height);
+static void default_framebuffer_size_callback(GLFWwindow *window, int width,
+                                              int height);
 
-Window::Window(uint width, uint height, const std::string& title, bool enable_msaa)
+Window::Window(uint width, uint height, const std::string &title,
+               bool enable_msaa)
     : _width(width), _height(height), _title(title) {
-    _window = create_window();
-    glfwMakeContextCurrent(_window);
-    // Sets a default basic frame buffer size callback
-    glfwSetFramebufferSizeCallback(_window, default_framebuffer_size_callback);
-    load_opengl_functions();
-    if (enable_msaa) {
-        glfwWindowHint(GLFW_SAMPLES, 4);
-        glEnable(GL_MULTISAMPLE);
-        LOG("MSAA enabled");
-    }
-    _loaded = true;
+  _window = create_window();
+  glfwMakeContextCurrent(_window);
+  // Sets a default basic frame buffer size callback
+  glfwSetFramebufferSizeCallback(_window, default_framebuffer_size_callback);
+  load_opengl_functions();
+  if (enable_msaa) {
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glEnable(GL_MULTISAMPLE);
+    LOG("MSAA enabled");
+  }
+  glViewport(0, 0, width * 2, height * 2);
+  _loaded = true;
 }
 
 Window::~Window() {
-    if (_loaded) {
-        glfwDestroyWindow(_window);
-    }
+  if (_loaded) {
+    glfwDestroyWindow(_window);
+  }
 }
 
-uint Window::width() const {
-    return _width;
-}
+uint Window::width() const { return _width; }
 
-uint Window::height() const {
-    return _height;
-}
+uint Window::height() const { return _height; }
 
-GLFWwindow* Window::data() {
-    return _window;
-}
+GLFWwindow *Window::data() { return _window; }
 
 bool Window::in_focus() const {
-    return glfwGetWindowAttrib(_window, GLFW_ICONIFIED) == 0;
+  return glfwGetWindowAttrib(_window, GLFW_ICONIFIED) == 0;
 }
 
-bool Window::should_close() const {
-    return glfwWindowShouldClose(_window);
-}
+bool Window::should_close() const { return glfwWindowShouldClose(_window); }
 
 void Window::enable_cursor() {
-    glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+  glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 }
 
 void Window::disable_cursor() {
-    glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 }
 
-GLFWwindow* Window::create_window() {
-    GLFWwindow* window = glfwCreateWindow(_width, _height, _title.c_str(), NULL, NULL);
-    ASSERT(window != nullptr, "Bad window intialization");
-    return window;
+GLFWwindow *Window::create_window() {
+  GLFWwindow *window =
+      glfwCreateWindow(_width, _height, _title.c_str(), NULL, NULL);
+  ASSERT(window != nullptr, "Bad window intialization");
+  return window;
 }
 
 void Window::load_opengl_functions() {
-    ASSERT(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress), "Bad glad initialization");
+  ASSERT(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress),
+         "Bad glad initialization");
 }
 
-static void default_framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
+static void default_framebuffer_size_callback(GLFWwindow *window, int width,
+                                              int height) {
+  glViewport(0, 0, width, height);
 }
-
